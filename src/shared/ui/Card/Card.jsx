17d10cf6b063133fc30052a22652615501361513
Destcable <artmobpavlov21@gmail.com>
2024-07-20 22:10:16 +0300

    [upd[ eslint & card component

--- src/shared/ui/Card/Card.jsx
+++ src/shared/ui/Card/Card.jsx
@@ -1,3 +1,8 @@
-export const Card = () => { 
-
+export const Card = ({ backgroundColor = "#ffff" }) => { 
+    return <div 
+        className="" 
+        style={{ backgroundColor: backgroundColor }}
+    >
+        123
+    </div>
 };
